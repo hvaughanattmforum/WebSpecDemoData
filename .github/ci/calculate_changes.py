057d23db428dc23e00b9fb7c60d26bad947891a4
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2023-11-30 17:07:08 +0100

    Testing diff

--- .github/ci/calculate_changes.py
+++ .github/ci/calculate_changes.py
@@ -2,7 +2,25 @@ from pathlib import Path
 import json
 import os
 
+FILE_CHANGES = Path(__file__).parents[2] / "filechanges"
+
+def read_file_changes():
+    with open(FILE_CHANGES, 'r') as f:
+        return f.readlines()
+
+
+def fileter_changes(changes):
+    for line in changes:
+        if "template" in line:
+            continue
+        if "specifications" in line:
+            yield line.strip()
+
 def main():
+    changes = read_file_changes()
+    changes = fileter_changes(changes)
+    print(list(changes))
+    
     return 0
 
 
