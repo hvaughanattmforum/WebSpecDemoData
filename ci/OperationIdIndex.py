edc2c163235605d7d4cafaa8f2e9359c90f16cae
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2024-07-28 17:08:39 -0500

    temp

--- ci/OperationIdIndex.py
+++ ci/OperationIdIndex.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 API_DOCS = Path(__file__).parents[3] / "APIS" / "api_table_docs" / "docs" / "ODA"
 ODA_INDEX = Path(__file__).parents[1] / "apiIndex.json"
-
+SPECS = Path(__file__).parents[1] / "specifications"
 
 class Swagger:
     def __init__(self, path, api_ref) -> None:
@@ -19,7 +19,6 @@ class Swagger:
             self.content = json.load(f)
 
     def extract_operation_ids(self):
-        print(f"Extracting {self.api_ref}")
         if "asyncapi" in self.content:
             return []
         operations = []
@@ -43,6 +42,56 @@ class OperationIndex:
             else:
                 self.index[operation].append(swagger.api_ref)
 
+class Component:
+    def __init__(self, path) -> None:
+        self.path = path
+        self.content = {}
+        self.versions = {
+            4.1: "v4.1.0",
+            4: "v4.0.0",
+            5: "v5.0.0"
+        }
+
+    def load_component(self):
+        with self.path.open("r") as f:
+            self.content = yaml.load(f, Loader=yaml.FullLoader)
+
+    def processEvents(self, operationIndex):
+        coreFunction = self.content["spec"]["coreFunction"]
+        events = [*coreFunction["publishedEvents"], *coreFunction["subscribedEvents"]]
+        print(self.path)
+        for event in events:
+            target_apis = []
+            #del event["port"]
+            #del event["call-back"]
+            #del event["implementation"]
+
+            for resource in event["resources"]:
+                opId_api = operationIndex[resource]
+                target_apis.extend(opId_api)
+            if len(target_apis) != 1:
+                print(target_apis)
+                print(self.path)
+
+
+
+    def core_function_required_apis(self):
+        apis = []
+        coreFunction = self.content["spec"]["coreFunction"]
+        for api in coreFunction["dependentAPIs"]:
+            if api["required"]:
+                apis.append(api)
+
+        for api in coreFunction["exposedAPIs"]:
+            if api["required"]:
+                apis.append(api)
+
+        for api in apis:
+            api_id = api["id"]
+            version = self.versions[api["version"]]
+            yield f"{api_id}_{version}"
+
+
 def load_swaggers():
     with ODA_INDEX.open("r") as f:
         index = json.load(f)
@@ -52,6 +101,12 @@ def load_swaggers():
         swagger = Swagger(swagger_path, api_ref)
         yield swagger
 
+def load_components():
+    for component in SPECS.glob("TMFC*/TMFC*.yaml"):
+        comp = Component(component)
+        comp.load_component()
+        yield comp
+        
 
 def build_index():
     operationIndex = OperationIndex()
@@ -60,7 +115,22 @@ def build_index():
         operationIndex.add_swagger(swagger)
 
 def main():
-    build_index()
+    #build_index()
+    with ODA_INDEX.open("r") as f:
+        index = json.load(f)
+    for component in load_components():
+        operationIndex = OperationIndex()
+        for api_ref in component.core_function_required_apis():
+            if api_ref not in index:
+                continue
+            swagger_name = Path(index[api_ref]["swagger"]).name
+            swagger_path = API_DOCS / swagger_name
+            swagger = Swagger(swagger_path, api_ref)
+            swagger.load_swagger()
+            operationIndex.add_swagger(swagger)
+        component.processEvents(operationIndex.index)
+        return
+
     return 0
 
 if __name__ == "__main__":
